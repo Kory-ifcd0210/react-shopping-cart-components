84d56567ee801fd57927da68a68a526034f4c5c4
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2022-02-09 15:58:28 +0000

    3

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,11 +2,14 @@ import React from "react";
 
 function Button({ submitButton, disabled = false, children, ...props }) {
   return (
-    <div className="btn btn-primary" type={submitButton ? "submit" : "button"}
-    disabled={disabled}
-      {...props}>
+    <button
+      className="btn btn-primary"
+      type={submitButton ? "submit" : "button"}
+      disabled={disabled}
+      {...props}
+    >
       {children}
-    </div>
+    </button>
   );
 }
 
